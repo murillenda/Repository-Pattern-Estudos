cc83621c43f1ae7276481bb47d5bde8029cb212d
Murillo Marques <giikthelol@gmail.com>
2023-05-17 21:07:07 -0300

    Desacoplando a implementacao de repository

--- src/main/java/com/github/murillenda/repository/RepositoryFactory.java
+++ src/main/java/com/github/murillenda/repository/RepositoryFactory.java
@@ -1,5 +1,7 @@
 package com.github.murillenda.repository;
 
+import com.github.murillenda.repository.mysql.MySQLVendaRepository;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -18,7 +20,7 @@ public class RepositoryFactory implements AutoCloseable {
     }
 
     public VendaRepository criarVendaRepository() {
-        return new VendaRepository(conexao);
+        return new MySQLVendaRepository(conexao);
     }
 
     @Override
--- src/main/java/com/github/murillenda/repository/VendaRepository.java
+++ src/main/java/com/github/murillenda/repository/VendaRepository.java
@@ -2,63 +2,10 @@ package com.github.murillenda.repository;
 
 import com.github.murillenda.entity.Venda;
 
-import java.math.BigDecimal;
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
-public class VendaRepository {
+public interface VendaRepository {
+    Venda adicionar(Venda venda);
 
-    private final Connection conexao;
-
-    public VendaRepository(Connection conexao) {
-        this.conexao = conexao;
-    }
-
-    public Venda adicionar(Venda venda) {
-        String dml = """
-            insert into venda (
-                nome_cliente,
-                valor_total,
-                data_pagamento
-            )
-            values (?, ?, ?)
-            """;
-
-        try (PreparedStatement comando = conexao.prepareStatement(dml, Statement.RETURN_GENERATED_KEYS)) {
-            comando.setString(1, venda.getNomeCliente());
-            comando.setBigDecimal(2, venda.getValorTotal());
-            comando.setDate(3, Date.valueOf(venda.getDataPagamento()));
-            comando.executeUpdate();
-
-            ResultSet codigoGeradoResultSet = comando.getGeneratedKeys();
-            codigoGeradoResultSet.next();
-            Long codigoGerado = codigoGeradoResultSet.getLong(1);
-
-            return new Venda(codigoGerado, venda.getNomeCliente(), venda.getValorTotal(), venda.getDataPagamento());
-        } catch (SQLException e) {
-            throw new PersistenciaException(e);
-        }
-    }
-
-    public List<Venda> consultar() {
-        var vendas = new ArrayList<Venda>();
-        try (Statement comando = conexao.createStatement();
-             var resultado = comando.executeQuery("select * from venda")) {
-
-            while (resultado.next()) {
-                Long id = resultado.getLong("id");
-                String nomeCliente = resultado.getString("nome_cliente");
-                BigDecimal valorTotal = resultado.getBigDecimal("valor_total");
-                Date dataPagamento = resultado.getDate("data_pagamento");
-
-                var venda = new Venda(id, nomeCliente, valorTotal, dataPagamento.toLocalDate());
-                vendas.add(venda);
-            }
-
-            return vendas;
-        } catch (SQLException e) {
-            throw new PersistenciaException(e);
-        }
-    }
+    List<Venda> consultar();
 }
